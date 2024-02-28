2274cb7b3c0b9107c099f967a033530061211cd9
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-02-28 10:33:58 +0100

    :sparkles: adding method shout and the test for it

--- src/rockStar.ts
+++ src/rockStar.ts
@@ -1,8 +1,13 @@
+import log from "./log";
 import { Music, Musician } from "./musician";
 
 export class RockStar extends Musician {
     constructor(firstName: string, lastName: string, age:number){
         super(firstName, lastName, age);
         this.style = Music.ROCK;
+    }
+    
+    shout(): void {
+        log("I'm shouting !")
     }
 }
--- tests/rock.spec.ts
+++ tests/rock.spec.ts
@@ -3,6 +3,13 @@ import { RockStar } from '../src/rockStar';
 
 describe('RockStar', () => {
 
+    let spy: jest.SpyInstance;
+
+    beforeEach(() => {
+        spy = jest.spyOn(console, 'log');
+        spy.mockClear();
+    });
+
     it('properties testing', () => {
         const musician = new RockStar('Mick', 'Jagger', 72);
         expect(musician.style).toBe(Music.ROCK);
@@ -12,5 +19,13 @@ describe('RockStar', () => {
         const musician = new RockStar('Mick', 'Jagger', 72);
         expect(musician.toString()).toBe('Mick Jagger plays ROCK');
     });
+
+    it("should return 'I'm swinging'", () => {
+        const musician = new RockStar('Mick', 'Jagger', 72);
+        
+        musician.shout();
+
+        expect(spy).toBeCalledTimes(1)
+    })
 
 });
